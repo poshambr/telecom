ad61c1585b99b362ce8abddbd5c3b6ff6b938ace
poshambr <bhargavareddy.posham1@gmail.com>
2018-05-04 10:12:03 -0700

    testing  of git file

---
+++ emp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Department,Designation,Employee,Intercom,Cug,Quarter
+
+
+admin.site.register(Department)
+admin.site.register(Designation)
+admin.site.register(Employee)
+admin.site.register(Intercom)
+admin.site.register(Cug)
+admin.site.register(Quarter)
+
+# Register your models here.
+
